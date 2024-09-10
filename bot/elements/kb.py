88bcde9f97f6f2d984e921b038ad466287f0314b
DiamondLand <simoforstudio@gmail.com>
2024-09-10 17:03:47 +0300

    Update kb.py

--- bot/elements/kb.py
+++ bot/elements/kb.py
@@ -1,15 +1,15 @@
 from aiogram.types import KeyboardButton, ReplyKeyboardMarkup
 
 dod_info = "День открытых дверей (ДОД)"
-current_info = " Актуальная информация"
 general_info = "Общая информация"
+communities_info = "Сообщества"
 specialties_info = "Специальности"
 
 # --- Главное меню --- #
 def main_menu_kb() -> ReplyKeyboardMarkup:
     kb = [
         [KeyboardButton(text=dod_info)], 
-        [KeyboardButton(text=current_info)],
+        [KeyboardButton(text=communities_info)],
         [KeyboardButton(text=general_info)],
         [KeyboardButton(text=specialties_info)],
     ]
